9d8996542838ab3f6648af38f4913918c927c71b
Roman <romabeorn@gmail.com>
2022-05-31 21:15:36 +0300

    Config File For Framework

---
+++ config.cpp
@@ -0,0 +1,42 @@
+#define _ARMA_
+
+class CfgPatches
+{
+	class RF
+	{
+		author = "Dad Fedor & Swift Dungeon";
+		name = "RF";
+		url = "https://discord.gg/d9kP7ezX3n";
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] = {"DZ_Data", "DZ_Sounds_Effects"};
+		requiredVersion = 0.1;
+	};
+};
+
+class CfgMods
+{
+	class RF
+	{
+		type="mod";
+		dir="RF";
+		class defs
+		{
+			class gameScriptModule
+			{
+				value = "";
+				files[] = { "RF/scripts/3_Game" };
+			};
+			class worldScriptModule
+			{
+				value = "";
+				files[] = { "RF/scripts/4_World" };
+			};
+			class missionScriptModule
+			{
+				value = "";
+				files[] = { "RF/scripts/5_Mission" };
+			};
+		};
+	};
+};
