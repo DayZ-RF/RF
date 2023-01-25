414193fe181159ac9e78044c4dc92121584544c4
Roman Matveev <romabeorn@gmail.com>
2023-01-25 15:23:02 +0300

    UUID

---
+++ scripts/3_Game/RF/Tools/UUID.c
@@ -0,0 +1,18 @@
+
+class RF_UUID: Managed {
+    static string New() {
+        autoptr array<string> chars = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
+        autoptr array<int> dashes = {8, 13, 18, 23};
+        string buf = "";
+
+        for (int i = 0; i < 36; i++) {
+            if (dashes.Find(i) == -1) {
+                buf = buf + chars.GetRandomElement();
+            } else {
+                buf = buf + "-";
+            }
+        }
+
+        return buf;
+    }
+}
