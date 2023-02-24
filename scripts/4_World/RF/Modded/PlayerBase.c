539fb619d6043729d22e2fe2e725be34241918dd
Roman Matveev <romabeorn@gmail.com>
2023-02-24 19:21:21 +0300

    GetSteamID64

---
+++ scripts/4_World/RF/Modded/PlayerBase.c
@@ -0,0 +1,23 @@
+modded class PlayerBase {
+
+	private bool RF_OnlineServicesInit;
+
+    string RF_GetSteamID64() {
+        if (!RF_OnlineServicesInit) {
+            OnlineServices.Init();
+            RF_OnlineServicesInit = true;
+        }
+        
+        BiosUserManager user_manager = GetGame().GetUserManager();
+        if (user_manager) {
+            if (user_manager.GetTitleInitiator()) {
+                user_manager.SelectUser(user_manager.GetTitleInitiator());
+            }
+        }
+
+        if (user_manager && user_manager.GetSelectedUser())
+            return user_manager.GetSelectedUser().GetUid();
+
+        return "-1";
+    }
+}
