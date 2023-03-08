46918a54161c6269b091db0b0082695b9daf6054
Roman Matveev <romabeorn@gmail.com>
2023-03-08 05:22:35 +0300

    Log

---
+++ scripts/3_Game/RF/Log/Log.c
@@ -0,0 +1,22 @@
+class RF_Log: Managed {
+
+    // MARK: - Constants
+
+    string GetModName() {
+        return "RF";
+    }
+
+    // MARK: - Internal
+
+    void Warning(string message) {
+        Print(string.Format("[%1][%2]: %3", GetModName(), "Warning", message));
+    }
+
+    void Info(string message) {
+        Print(string.Format("[%1][%2]: %3", GetModName(), "Info", message));
+    }
+
+    void Error(string message) {
+        Print(string.Format("[%1][%2]: %3", GetModName(), "Error", message));
+    }
+}
