b0fb235caf8a28ec777d2961ebd46f4119ec8e95
Roman Matveev <romabeorn@gmail.com>
2023-03-16 21:54:55 +0300

    Форматирование чисел в дате

--- scripts/3_Game/RF/Tools/TimeStampFormatter.c
+++ scripts/3_Game/RF/Tools/TimeStampFormatter.c
@@ -11,11 +11,11 @@ class RF_TimeStampFormatter: Managed {
 	string Formatted(RF_TimeStamp timeStamp) {
 		string weekday = weekdays[timeStamp.GetWeekday() - 1];
 		string month = months[timeStamp.GetMonth() - 1];
-		int day = timeStamp.GetDay();
-		int year = timeStamp.GetYear();
-		int hour = timeStamp.GetHour();
-		int minute = timeStamp.GetMinute();
-		int second = timeStamp.GetSecond();
+		string day = ToString(timeStamp.GetDay());
+		string year = ToString(timeStamp.GetYear());
+		string hour = ToString(timeStamp.GetHour());
+		string minute = ToString(timeStamp.GetMinute());
+		string second = ToString(timeStamp.GetSecond());
 		array<int> gmt = timeStamp.GetGMT();
 		string gmtString = gmt[0].ToString() + gmt[1].ToString() + gmt[2].ToString() + gmt[3].ToString();
 		string sign = "+";
@@ -23,5 +23,10 @@ class RF_TimeStampFormatter: Managed {
 			sign = "-";
 		}
 		return string.Format("%1 %2 %3 %4 %5:%6:%7 GMT%8%9", weekday, month, day, year, hour, minute, second, sign, gmtString);
+	}
+
+	static string ToString(int value) {
+		if (value < 10) return string.Format("0%1", value);
+		return string.Format("%1", value);
 	}
 }
