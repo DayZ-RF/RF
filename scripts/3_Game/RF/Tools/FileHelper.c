c23448233da3106ba15543b14972d9ffd946ce9d
patison5 <patison4@yandex.ru>
2026-03-01 18:21:21 +0100

    add file manager helper

---
+++ scripts/3_Game/RF/Tools/FileHelper.c
@@ -0,0 +1,24 @@
+class RF_File: Managed {
+
+	static TStringArray GetFilesList(string folder, string type = "json") {
+	    string fileName;
+	    int fileAttr, flags;
+	    bool found = true;
+	    TStringArray list = {};
+
+	    if (!FileExist(folder))
+	        MakeDirectory(folder);
+	    
+	    string path_find_pattern = folder + string.Format("/*.%1", type);
+	    FindFileHandle file_handler = FindFile(path_find_pattern, fileName, fileAttr, flags);
+	    
+	    while (found) {
+	        int pos = fileName.IndexOf(".");
+	        if (pos > -1)
+	            list.Insert(fileName.Substring(0, pos));
+	        found = FindNextFile(file_handler, fileName, fileAttr);
+	    }
+
+	    return list;
+	}
+}
